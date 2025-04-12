2e1bce3f93bea91db5ac5e06d0b2d6770171d6f2
Yatharth Giri <yatharth@example.com>
2025-04-12 23:16:12 +0530

    updated model

--- backend/src/models/Resume.js
+++ backend/src/models/Resume.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
+// Define the Resume schema
 const ResumeSchema = new mongoose.Schema(
   {
     jobTitle: { type: String, required: true },
     experience: { type: Number, required: true },
     skills: { type: [String], required: true },
+    education: { type: String, required: true },
     content: { type: String, required: true },
   },
+
+  // Add timestamps to the schema for createdAt and updatedAt fields
   { timestamps: true }
 );
 
