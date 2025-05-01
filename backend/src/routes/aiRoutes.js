516782ad3acd46930820f4fcbc8e0d04ddb7e055
Yatharth Giri <yatharth@example.com>
2025-05-01 22:01:31 +0530

    updated resumeGeneration and routes

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const aiController = require("../controllers/aiController");
+const resumeAiController = require("../controllers/resumeAiController");
+// const coverLetterAiController = require("../controllers/coverLetterAiController");
 
 // Route to generate a resume
-router.post("/generate-resume", aiController.generateResume);
+router.post("/generate-resume", resumeAiController.generateResume);
 
+// Cover letter generation route
+// router.post(
+//   "/generate-cover-letter",
+//   coverLetterAiController.generateCoverLetter
+// );
 module.exports = router;
